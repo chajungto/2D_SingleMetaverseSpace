376c102818aab06091b9ff4ea97e15ebc1e3b0dd
chajungto <50168549+chajungto@users.noreply.github.com>
2025-02-19 19:11:35 +0900

    [Add] Flappy Bird 장애물 / 배경 반복 구현
    Flappy Bird 장애물 / 배경 반복 구현

---
+++ Assets/Scripts/FlappyBirdGame/BGLooper.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class BGLooper : MonoBehaviour
+{
+    //장애물 카운트
+    public int obstacleCount = 0;
+
+    //블럭 카운트
+    public int blockCount = 17;
+
+    //배경 카운트
+    public int numBgCount = 10;
+
+    public Vector3 obstacleLastPosition = Vector3.zero;
+
+    void Start()
+    {
+        Obstacle[] obstacles = GameObject.FindObjectsOfType<Obstacle>();
+
+        obstacleLastPosition = obstacles[0].transform.position;
+        obstacleCount = obstacles.Length;
+
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            obstacleLastPosition = obstacles[i].SetRandomPlace(obstacleLastPosition, obstacleCount);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Obstacle obstacle = collision.GetComponent<Obstacle>();
+        if (obstacle)
+        {
+            obstacleLastPosition = obstacle.SetRandomPlace(obstacleLastPosition, obstacleCount);
+        }
+
+        if (collision.CompareTag("Boundary"))
+        {
+            float widthOfBgObject = ((BoxCollider2D)collision).size.x;
+            Vector3 pos = collision.transform.position;
+
+            pos.x += widthOfBgObject * blockCount * numBgCount;
+            collision.transform.position = pos;
+
+            //Debug.Log(pos);
+            return;
+        }
+    }
+}
--- Assets/Scripts/FlappyBirdGame/Obstacle.cs
+++ Assets/Scripts/FlappyBirdGame/Obstacle.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float highPosY = 1f;
+    public float lowPosy = -1f;
+
+    //위와 아래의 장애물 간격
+    public float holeMinSize = 1f;
+    public float holeMaxSize = 3f;
+
+    //위 아래 장애물
+    public Transform topObject;
+    public Transform bottomObject;
+
+    //장애물들 사이의 폭
+    public float widthPadding = 4f;
+
+    //랜덤위치 장애물 생성
+    public Vector3 SetRandomPlace(Vector3 lastPosition, int obstaclecount)
     {
-        
+        float holeSize = Random.Range(holeMinSize, holeMaxSize);
+        float halfHoleSize = holeSize / 2f;
+
+        topObject.localPosition = new Vector3(0, halfHoleSize + 2f);
+        bottomObject.localPosition = new Vector3(0, -halfHoleSize);
+
+        Vector3 placePosition = lastPosition + new Vector3(widthPadding, 0);
+        placePosition.y = Random.Range(lowPosy, highPosY);
+
+        transform.position = placePosition;
+        return placePosition;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
     void Update()
     {
-        
+
     }
 }
